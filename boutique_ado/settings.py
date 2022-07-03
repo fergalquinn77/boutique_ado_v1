a678bfb948a7427369fb8fc9649150f206932b69
fergalquinn77 <fergal@bourbon.ie>
2022-07-03 09:22:38 +0000

    Debug False

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ci-fq-boutique-ado.herokuapp.com', 'localhost']
 
